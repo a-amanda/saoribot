45943bcdbe3040d9521cf5fac1f902b66b9e8f13
Tamura <85198916+Titio-Tamura@users.noreply.github.com>
2021-12-29 12:29:59 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const config = {
-  token: 'Nzk0OTY4MjcwMjU0ODk5MjQw.X_Ch5Q.33DNeMKZd_BeIZR1GaRNGiTT154',
+  token: 'Nzk0OTY4MjcwMjU0ODk5MjQw.X_Ch5Q.bYQs6_qeOcoOC6gcCJq-xrNytHY',
   prefix: 'sa!'
 }
 module.exports = config;
